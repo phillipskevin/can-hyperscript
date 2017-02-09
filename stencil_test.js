46e44d379cafb292a5663436cd58cf106193e478
Kevin Phillips <kphillips86@gmail.com>
2017-02-08 21:16:23 -0600

    moving code to lib/

--- stencil_test.js
+++
@@ -1,9 +0,0 @@
-import QUnit from 'steal-qunit';
-import plugin from './stencil';
-
-QUnit.module('stencil');
-
-QUnit.test('Initialized the plugin', function(){
-  QUnit.equal(typeof plugin, 'function');
-  QUnit.equal(plugin(), 'This is the stencil plugin');
-});
--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
-import '../stencil_test';
+import QUnit from 'steal-qunit';
+import stencil from '../lib/stencil';
+
+QUnit.module('stencil');
+
+QUnit.test('stencil', function(){
+  QUnit.equal(typeof stencil, 'function');
+  QUnit.equal(stencil(), 'This is the stencil plugin');
+});
