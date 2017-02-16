4b9d8e5828b06290376b231b675236ef0e6cf7a3
Kevin Phillips <kphillips86@gmail.com>
2017-02-16 15:47:26 -0600

    using proxy scope and view function

--- examples/can-component/index.js
+++ examples/can-component/index.js
@@ -1,25 +1,36 @@
 import Component from 'can-component';
 import stache from 'can-stache';
 import DefineMap from 'can-define/map/map';
-import { h1, text } from '../../lib/stencil';
+
+import stencil, { h1 } from '../../lib/stencil';
 
 import './styles.css';
-
-const view = h1({
-	class: '{class}'
-}, [
-	'Hello, {message}!'
-]);
 
 const ViewModel = DefineMap.extend({
 	message: { value: 'World' },
 	class: { value: 'big-h1' }
 });
 
+const view = (scope) => {
+	return h1({
+		class: scope.class
+	}, [
+		`Hello, ${scope.message}!`
+	]);
+};
+
 Component.extend({
 	tag: 'hello-world',
 	ViewModel,
-	view
+	view: stencil(view),
+	events: {
+		inserted() {
+			setTimeout(() => {
+				this.viewModel.message = 'Kevin';
+				this.viewModel.class = 'bigger-h1';
+			}, 2000);
+		}
+	}
 });
 
 document.body.append(
--- test/test.js
+++ test/test.js
@@ -1,22 +1,30 @@
 import QUnit from 'steal-qunit';
 import SimpleMap from 'can-simple-map';
-import { h1 } from '../lib/stencil';
+import stencil, { h1 } from '../../lib/stencil';
 
 QUnit.module('stencil');
 
 QUnit.test('basics', () => {
-	const scope = new SimpleMap({ msg: 'World', class: 'big-h1' });
-	const template = h1({
-		class: '{class}'
-	}, [
-		'Hello, {msg}!'
-	]);
+	const scope = new SimpleMap({
+		message: 'World',
+		class: 'big-h1'
+	});
+
+	const view = (scope) => {
+		return h1({
+			class: scope.class
+		}, [
+			`Hello, ${scope.message}!`
+		]);
+	};
+
+	const template = stencil(view);
 	const frag = template(scope);
 	QUnit.equal(frag.firstChild.className, 'big-h1');
 	QUnit.equal(frag.firstChild.tagName, 'H1');
 	QUnit.equal(frag.firstChild.innerHTML, 'Hello, World!');
 
-	scope.attr('msg', 'Mars');
+	scope.attr('message', 'Mars');
 	scope.attr('class', 'small-h1');
 	QUnit.equal(frag.firstChild.className, 'small-h1');
 	QUnit.equal(frag.firstChild.tagName, 'H1');
